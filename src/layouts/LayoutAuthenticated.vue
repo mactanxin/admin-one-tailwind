c91e69351aba79c17443b5796899b1085e558025
Xin Tan <mactanxin@gmail.com>
2022-08-14 21:58:48 +0800

    Initial commit

---
+++ src/layouts/LayoutAuthenticated.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { layoutAsidePadding } from '@/config.js'
+import menuAside from '@/menuAside.js'
+import menuNavBar from '@/menuNavBar.js'
+import { useMainStore } from '@/stores/main.js'
+import { useLayoutStore } from '@/stores/layout.js'
+import { useStyleStore } from '@/stores/style.js'
+import NavBar from '@/components/NavBar.vue'
+import AsideMenu from '@/components/AsideMenu.vue'
+import FooterBar from '@/components/FooterBar.vue'
+
+useMainStore().setUser({
+  name: 'John Doe',
+  email: 'john@example.com',
+  avatar: 'https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93'
+})
+
+const styleStore = useStyleStore()
+
+const layoutStore = useLayoutStore()
+
+const menuClick = (event, item) => {
+  if (item.isToggleLightDark) {
+    styleStore.setDarkMode()
+  }
+
+  if (item.isLogout) {
+    //
+  }
+}
+</script>
+
+<template>
+  <div :class="{ 'dark': styleStore.darkMode, 'overflow-hidden lg:overflow-visible': layoutStore.isAsideMobileExpanded }">
+    <div
+      :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': layoutStore.isAsideMobileExpanded }]"
+      class="pt-14 w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100"
+    >
+      <NavBar
+        :menu="menuNavBar"
+        :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': layoutStore.isAsideMobileExpanded }]"
+        @menu-click="menuClick"
+      />
+      <AsideMenu
+        :menu="menuAside"
+        @menu-click="menuClick"
+      />
+      <slot />
+      <FooterBar />
+    </div>
+  </div>
+</template>
